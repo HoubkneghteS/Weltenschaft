e4b85387f4690e3b1afbfc44b7bc8aba3ec8c52a
Adam Simons <34462040+HoubkneghteS@users.noreply.github.com>
2019-12-30 15:03:04 -0600

    minor improvements

--- translate.js
+++ translate.js
@@ -1,2 +1,7 @@
 const en = require("./en.json")
     de = require("./de.json");
+    //jsons for the strings
+
+function langSet(lang) {
+    document.getElementsByTagName("html").lang = lang; //sets content language to whatever language is being used
+}
