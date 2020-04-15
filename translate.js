76e7107dc56e0b764d4b4304f632d209a952ccf8
HoubkneghteS <34462040+HoubkneghteS@users.noreply.github.com>
2020-04-15 04:24:57 -0500

    translations now work behind the scenes
    the setLang() function now will set everything to the locale

--- translate.js
+++ translate.js
@@ -1,7 +1,19 @@
-const en = require("./en.json")
-    de = require("./de.json") //jsons for the strings
-    lang = require("./lang.json"); //detects system language
+const lang = require("./lang.json"); //detects system language
 
-function langSet() {
+function setLang() {
     document.getElementsByTagName("html").lang = lang; //sets content language to whatever language is being used
+
+    //sets local locale object from JSON
+    if(lang == "de"){
+        var locale = require("./de.json");
+    }else{
+        var locale = require("./en.json"); 
+    }
+
+    //replaces text with the value found in the locale JSON
+    for (var string in locale) {
+        if (document.getElementById(string)) {
+            document.getElementById(string).innerHTML = locale[string];
+        }
+    }
 }
