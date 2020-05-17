af1f2337812524f3974cc7c4df4e95f1934b3056
HoubkneghteS <adam9812@live.com>
2020-05-17 03:46:18 -0500

    further optimizations to translation engine

--- translate.js
+++ translate.js
@@ -6,12 +6,9 @@ function setLang() {
     document.getElementsByTagName("html").lang = lang; //sets content language to whatever language is being used
 
     //sets locale object from JSON
-    if (fs.existsSync(__dirname + `/locales/${lang}.json`)) {
-        const loadedLanguage = JSON.parse(fs.readFileSync(__dirname + `/locales/${lang}.json`), 'utf8');
-    }
-    else {
-        const loadedLanguage = JSON.parse(fs.readFileSync(__dirname + `/locales/en.json`), 'utf8');
-    }
+    const loadedLanguage = fs.existsSync(`./locales/${lang}.json`)
+        ? JSON.parse(fs.readFileSync(`./locales/${lang}.json`), 'utf8')
+        : JSON.parse(fs.readFileSync(`./locales/en.json`), 'utf8');
 
     //replaces text with the value found in the locale JSON
     for (var string in loadedLanguage) {
