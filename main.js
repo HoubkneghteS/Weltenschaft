08d1cd5c81de7cfb625fde93b4170a4262c1d607
HoubkneghteS <adam9812@live.com>
2020-05-30 05:11:11 -0500

    organization

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 
 var mainWindow, infoWindow, settingsWindow; //window setup
 
-var locale;
-
-//create menu template
-var menuTemplate;
+var locale,
+	menuTemplate;
 
 //startup functions
 app.on('ready', () => {
@@ -92,10 +90,30 @@ app.on('ready', () => {
 	});
 });
 
-//Detects settings and sends it to main window
-ipcMain.on("setting", (e, ...args) => {
-	mainWindow.webContents.send("setting", args);
-});
+//mac compatibility
+if (process.platform == 'darwin') {
+	menuTemplate.unshift({});
+}
+
+//sets locale object from JSON
+function getLocaleObject(src = app.getLocale()){
+	const fs = require('fs');
+
+	//searches places where the file could be
+	if (fs.existsSync(`./locales/${src}.json`)) return JSON.parse(fs.readFileSync(`./locales/${src}.json`));
+	if (fs.existsSync(`./resources/app/locales/${src}.json`)) return JSON.parse(fs.readFileSync(`./resources/app/locales/${src}.json`));
+
+	//if lang.json does not exist use english as default
+	if (fs.existsSync(`./locales/en.json`)){
+		console.warn(`Locale for language: ${src} not detected - using English`);
+		return JSON.parse(fs.readFileSync(`./locales/en.json`));
+	}else{
+		console.warn(`Locale for language: ${src} not detected - using English`);
+		return JSON.parse(fs.readFileSync(`./resources/app/locales/en.json`));
+	}
+}
+
+/* WINDOWS */
 
 //main window
 function createMainWindow() {
@@ -156,7 +174,7 @@ function createInfoWindow() {
 		e.preventDefault();
 		shell.openExternal(url);
 	});
-};
+}
 
 //settings window
 function createSettingsWindow() {
@@ -192,28 +210,12 @@ function createSettingsWindow() {
 	});
 }
 
-//mac compatibility
-if (process.platform == 'darwin') {
-	menuTemplate.unshift({});
-}
+/*INTERPROCESS COMMUNICATION*/
 
-//sets locale object from JSON
-function getLocaleObject(src = app.getLocale()){
-	const fs = require('fs');
-
-	//searches places where the file could be
-	if (fs.existsSync(`./locales/${src}.json`)) return JSON.parse(fs.readFileSync(`./locales/${src}.json`));
-	if (fs.existsSync(`./resources/app/locales/${src}.json`)) return JSON.parse(fs.readFileSync(`./resources/app/locales/${src}.json`));
-
-	//if lang.json does not exist use english as default
-	if (fs.existsSync(`./locales/en.json`)){
-		console.warn(`Locale for language: ${src} not detected - using English`);
-		return JSON.parse(fs.readFileSync(`./locales/en.json`));
-	}else{
-		console.warn(`Locale for language: ${src} not detected - using English`);
-		return JSON.parse(fs.readFileSync(`./resources/app/locales/en.json`));
-	}
-}
+//Detects settings and sends it to main window
+ipcMain.on("setting", (e, ...args) => {
+	mainWindow.webContents.send("setting", args);
+});
 
 //sends locale to translate.js
 ipcMain.handle('getLang', async(e, language) => getLocaleObject(language));
