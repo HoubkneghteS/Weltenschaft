ab418a7a77331c479eaeb8f9b9c67347f343e79a
HoubkneghteS <adam9812@live.com>
2020-05-30 06:51:18 -0500

    making the function self-calling for less redundancy

--- translate.js
+++ translate.js
@@ -1,4 +1,4 @@
-async function setLang(language) {
+(async function setLang(language) {
 	const {ipcRenderer} = require('electron');
 	const locale = await ipcRenderer.invoke('getLang', language);
 	document.getElementsByTagName("html").lang = language; //sets content language to whatever language is being used
@@ -9,4 +9,4 @@ async function setLang(language) {
 			document.getElementById(string).innerHTML = locale[string];
 		}
 	}
-}
+})()
