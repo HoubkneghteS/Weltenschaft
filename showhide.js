4d3d830c43e7d8d47b4c6c2fb525507d3feffc3c
HoubkneghteS <adam9812@live.com>
2020-08-03 01:55:53 -0500

    moving showhide code to own script so it can be used elsewhere

---
+++ showhide.js
@@ -0,0 +1,11 @@
+function toggleShow(){
+    if(document.getElementById("showmore")){
+        document.getElementById("showmore").id = "showless";
+        document.getElementById("advanced").style.display = "block";
+    }else{
+        document.getElementById("showless").id = "showmore";
+        document.getElementById("advanced").style.display = "none";
+    }
+    //changes showmore/showless when the id changes
+    setLang();
+}
